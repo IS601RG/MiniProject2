67a674bc043b7af39d86cb96dbe1460fab907d21
riya <rg329@njit.edu>
2018-10-14 14:51:58 -0400

    Fixed: Moved the footer to the bottom Added: style

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,11 @@
 <footer class="container">
-    <p class="float-right"><a href="#">Back to top</a></p>
-    <p>&copy; Created by: Riya Gandhi &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+
+    <div class="container" style="height:100% position:relative;">
+        <div style="height=10%; position:absolute; bottom:20px;">
+            <p class="float-left">&copy; Created by: Riya Gandhi &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+
+        </div>
+
+
+    </div>
 </footer>
