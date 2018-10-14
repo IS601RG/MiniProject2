0c6660e7d93494d0401e699ce29263c2810fd136
riya <rg329@njit.edu>
2018-10-14 00:56:47 -0400

    Task: Initial Project Setup

---
+++ readme.md
@@ -0,0 +1,4 @@
+#My Mini Project 2 for IS601
+
+1. Intall Laravel
+2. Create 2 page website
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.default')
+@section('content')
+
+    <h1>Welcome to my Homepage</h1>
+@stop
