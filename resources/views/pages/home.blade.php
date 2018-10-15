123a24d53973e2c3d211b38c448e10621b64741e
riya <rg329@njit.edu>
2018-10-14 21:55:53 -0400

    Update: errors fixed. Heroku up and running

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="card-body" style="margin-top: 40px">
         <div class="card mt-5 pl-5 pr-20" >
-            <h1 style="style-align: center">Welcome to my Homepage</h1>
+            <h1 style="style-align: center">Welcome to My Homepage</h1>
 
         </div>
     </div>
